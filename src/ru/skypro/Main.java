91ac613abad2ec5fa968afb21d39476a183a714b
ved7997 <ved7997@gmail.com>
2022-01-22 21:59:25 +0300

    Не выходит каменный цветок

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,59 +5,112 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-                //Задание №1
+        //Задание №1
 
-        visocos(1999);
+        visocos(1980);
 
-                //Задание №2
+        //Задание №2
 
         operatingSystem(2002, 1);
-
+//        int clientOS = 0;
+//        int clientDeviceYear = 2021;
+//        int currentYear = LocalDate.now().getYear();
+//        if (clientDeviceYear == currentYear) {
+//
+//            if (clientOS == 1) {
+//                System.out.println("Установите версию приложения для Android по ссылке");
+//
+//            } else if (clientOS == 0) {
+//                System.out.println("Установите версию приложения для iOS по ссылке");
+//
+//            }
+//
+//        }
+//        String ios = "Установите облегченную версию приложения для iOS по ссылке";
+//        String android = "Установите облегченную версию приложения для Android по ссылке";
+//
+//        if (clientDeviceYear < currentYear) {
+//
+//            if (clientOS == 1) {
+//                System.out.println(android);
+//            } else if (clientOS == 0) {
+//                System.out.println(ios);
+//            }
+//        }
     }
 
-    public static int visocos(int year ) {
+    public static int visocos(int year) {
         if (year % 4 == 0 || year % 100 == 0 || year % 400 == 0) {
-           System.out.println(year + " год является високосным ");
+            System.out.println(year + " год является високосным ");
             return year;
         } else {
-         System.out.println(year + " год не является високосным ");
-                return year;
+            System.out.println(year + " год не является високосным ");
+            return year;
         }
-
     }
 
-    public static int operatingSystem(int clientDeviceYear, int clientOS ){
+
+    public static String operatingSystem(int clientDeviceYear, int clientOS) {
+
+
+        //int clientOS = ;
+        //int clientDeviceYear = 2021;
+        String stro7;
+        String stro1 = "Установите версию приложения для Android по ссылке";
+        String stro2 = "Установите версию приложения для iOS по ссылке";
+        String stro3 = "Установите облегченную версию приложения для iOS по ссылке";;
+        String stro4 = "Установите облегченную версию приложения для Android по ссылке";;
+        //System.out.println(stro7);
+
         int currentYear = LocalDate.now().getYear();
-        if (clientDeviceYear >= currentYear) {
+        if (clientDeviceYear == currentYear) {
 
             if (clientOS == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-              // return clientDeviceYear;
-              // return clientOS;
+                //String stro1 = "Установите версию приложения для Android по ссылке";
+                stro7 = stro1;
+                System.out.println(stro7);
+                return stro7;
+
+                //System.out.println(stro1);
+
             } else if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-               // return clientDeviceYear;
-                //return clientOS;
+                //String stro2 = "Установите версию приложения для iOS по ссылке";
+//                System.out.println(stro2);
+//                return stro2;
+                stro7 = stro2;
+                System.out.println(stro7);
+                return stro7;
             }
-            //return clientDeviceYear;
-            return clientOS;
+
         }
+//        String ios = "Установите облегченную версию приложения для iOS по ссылке";
+//        String android = "Установите облегченную версию приложения для Android по ссылке";
 
         if (clientDeviceYear < currentYear) {
 
             if (clientOS == 1) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-               // return clientDeviceYear;
-                //return clientOS;
+                stro7 = stro3;
+                System.out.println(stro7);
+                return stro7;
+
             } else if (clientOS == 0) {
-              System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                //return clientDeviceYear;
-                //return clientOS;
+               // System.out.println(ios);
+                stro7 = stro4;
+                System.out.println(stro7);
+
+
             }
-            //return clientDeviceYear;
-           return clientOS;
+
+
         }
 
+
+
+
+
+
     }
+
+
 
 }
